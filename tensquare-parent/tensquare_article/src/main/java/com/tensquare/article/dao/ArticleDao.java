40d37eec969d8704adc4b04a210af10f1d1a3b58
yangzhengxing <1010621868@qq.com>
2018-12-26 22:59:43 +0800

    www.weifuwukt.com 文章微服务 作者:杨郑兴

---
+++ tensquare-parent/tensquare_article/src/main/java/com/tensquare/article/dao/ArticleDao.java
@@ -0,0 +1,26 @@
+package com.tensquare.article.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.tensquare.article.pojo.Article;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface ArticleDao extends JpaRepository<Article,String>,JpaSpecificationExecutor<Article>{
+
+    //文章审核 state 1：审核通过，0：审核不通过
+    @Modifying
+    @Query(value = "UPDATE tb_article SET state=? WHERE id =?",nativeQuery = true)
+    public void updateState(String state,String id);
+
+    //更新文章点赞数
+    @Modifying
+    @Query(value = "UPDATE tb_article SET thumbup=thumbup+1 WHERE id = ?",nativeQuery = true)
+    public void addThumbup(String id);
+}
