5875b2fa09e2b4619a1956c8428a41eafa12e772
yangzhengxing <1010621868@qq.com>
2019-01-05 02:35:42 +0800

    www.weifuwukt.com 注册中心微服务 作者:杨郑兴

---
+++ tensquare-parent/tensquare-eureka/src/main/java/com/tensquare/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.tensquare.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 杨郑兴
+ * @Date 2019/1/2 0:03
+ * @官网 www.weifuwukt.com
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
